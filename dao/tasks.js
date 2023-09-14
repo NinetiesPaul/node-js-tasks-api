c74c20743b35eda7c03f3357b87b8019abc98ac4
Paul Richard <paulrpm@gmail.com>
2023-09-14 00:24:49 -0300

    dao classes clean up

--- dao/tasks.js
+++ dao/tasks.js
@@ -5,27 +5,21 @@ const connection = require('../db/connection.js')
 const Tasks = connection.define('tasks', {
     title: {
         type: Sequelize.STRING,
-        allowNull: false,
     },
     description: {
         type: Sequelize.STRING,
-        allowNull: false,
     },
     status: {
         type: Sequelize.STRING,
-        allowNull: false,
     },
     type: {
         type: Sequelize.STRING,
-        allowNull: false,
     },
     createdOn: {
         type: Sequelize.DATE,
-        allowNull: false,
     },
     createdBy: {
         type: Sequelize.INTEGER,
-        allowNull: false,
     },
     closedOn: {
         type: Sequelize.DATE,
--- dao/users.js
+++ dao/users.js
@@ -5,16 +5,12 @@ const connection = require('../db/connection.js')
 const Users = connection.define('users', {
     name: {
         type: Sequelize.STRING,
-        allowNull: false,
     },
     email: {
         type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
     },
     password: {
         type: Sequelize.STRING,
-        allowNull: false,
     }
 })
 
