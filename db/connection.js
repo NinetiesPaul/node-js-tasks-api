ed7ea59febd981bdc3597598e9014b57d1be900e
Paul Richard <paulrpm@gmail.com>
2025-06-01 02:02:43 -0300

    feat: added support to postgresql

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require ('sequelize');
 
 const connection = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: process.env.DB_ENGINE,
     port: process.env.DB_PORT,
     define: {
         timestamps: false
